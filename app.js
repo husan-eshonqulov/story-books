69c64c52df401ecb6dd0fe8e6a44bd656e89d389
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-03-21 16:18:25 +0500

    print user profile

---
+++ app.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const { engine } = require('express-handlebars');
+
+const connectDB = require('./config/db');
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes'));
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
+  )
+);
