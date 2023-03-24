3583f6ad3ce0d42983da11858d06b7dc5b6689fb
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-03-24 10:51:38 +0500

    add truncate and stipTags

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MongoStore = require('connect-mongo');
 const { engine } = require('express-handlebars');
 
 const connectDB = require('./config/db');
-const { formatDate } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate } = require('./helpers/hbs');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
@@ -33,7 +33,11 @@ if (process.env.NODE_ENV === 'development') {
 // Handlebars
 app.engine(
   '.hbs',
-  engine({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' })
+  engine({
+    helpers: { formatDate, stripTags, truncate },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
 );
 app.set('view engine', '.hbs');
 
--- helpers/hbs.js
+++ helpers/hbs.js
@@ -4,4 +4,20 @@ module.exports = {
   formatDate: function (date, format) {
     return moment(date).format(format);
   },
+
+  truncate: function (str, len) {
+    if (str.length > len && str.length > 0) {
+      let newStr = str + ' ';
+      newStr = str.substr(0, len);
+      newStr = str.substr(0, newStr.lastIndexOf(' '));
+      newStr = newStr.length > 0 ? newStr : str.substr(0, len);
+      return newStr + '...';
+    } else {
+      return str;
+    }
+  },
+
+  stripTags: function (input) {
+    return input.replace(/<(?:.|\n)*?>/gm, '');
+  },
 };
