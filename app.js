9cdd77a691843deab0035854d53f0db147674b7a
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-03-24 12:03:49 +0500

    add update story

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 
 const connectDB = require('./config/db');
@@ -30,6 +31,18 @@ const app = express();
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
